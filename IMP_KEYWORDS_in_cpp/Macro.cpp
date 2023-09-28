9b849e4db6d44e0c336e686922d9a73564b9693c
Abhishek <abhisaloke2424@gmail.com>
2023-09-28 16:55:26 +0530

    first commit

---
+++ IMP_KEYWORDS_in_cpp/Macro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// defining macro
+#define PI 3.14;
+
+int main(){
+
+    int r = 1;
+    double area =  PI ;
+    
+    cout<<area<<endl;
+    return 0;
+}
